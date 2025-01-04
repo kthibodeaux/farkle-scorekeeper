1c88745b8be118c10284c4b4ef1d3822b803921c
Keith Thibodeaux <keith@railscoder.net>
2025-01-04 13:20:44 -0500

    add dieface component

---
+++ src/components/DieFace.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  side: Number,
+})
+
+const diceClasses = computed(() => {
+  return {
+    dice: true,
+    [`dice-${props.side}`]: true,
+  }
+})
+</script>
+
+<template>
+  <span :class="diceClasses"></span>
+</template>
