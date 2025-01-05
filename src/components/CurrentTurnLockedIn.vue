d1d90d5264cd4d622d1943ba9ec63fd1dee7b571
Keith Thibodeaux <keith@railscoder.net>
2025-01-05 11:07:37 -0500

    disable undo last instead of hiding

--- src/components/CurrentTurnLockedIn.vue
+++ src/components/CurrentTurnLockedIn.vue
@@ -8,10 +8,11 @@ const currentTurn = useCurrentTurnStore()
 </script>
 
 <template>
-  <div v-if="currentTurn.canUndo">
+  <div>
     <br />
     <br />
-    <input type="submit" class="secondary" @click="currentTurn.undo" value="Undo Last" />
+    <input :disabled="!currentTurn.canUndo" type="submit" class="secondary" @click="currentTurn.undo"
+      value="Undo Last" />
   </div>
   <div v-for="(roll, index) in currentTurn.lockedIn" :key="index">
     <DieFace v-for="(side, index) in roll" :key="index" :side="side" size="1em" :clickable="false" />
